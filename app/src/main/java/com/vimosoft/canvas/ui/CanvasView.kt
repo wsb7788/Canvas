bce56aa01d0fe8b62894e321800d55841242dc38
Won SeungBin <wsb0514@naver.com>
2024-07-29 16:49:11 +0900

    [DESIGN] CanvasActivity 레이아웃 설정

---
+++ app/src/main/java/com/vimosoft/canvas/ui/CanvasView.kt
@@ -0,0 +1,12 @@
+package com.vimosoft.canvas.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CanvasView(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : View(context, attrs) {
+
+}
---
+++ app/src/main/java/com/vimosoft/canvas/ui/ColorPickerView.kt
@@ -0,0 +1,11 @@
+package com.vimosoft.canvas.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ColorPickerView(
+    context: Context,
+    attrs: AttributeSet? = null,
+): View(context, attrs) {
+}
